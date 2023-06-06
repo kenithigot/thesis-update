5c0a528601c63488949058998f00a98225700e82
kenithigot <kennithigot@gmail.com>
2023-06-06 22:39:09 +0800

    adding some codes

--- overview.php
+++ overview.php
@@ -62,6 +62,16 @@
                         aria-selected="true">Introduction</button>
                 </li>
                 <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                        aria-selected="true">Introduction</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                        aria-selected="true">Introduction</button>
+                </li>
+                <li class="nav-item" role="presentation">
                     <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
                         data-bs-target="#pills-profile" type="button" role="tab"
                         aria-controls="pills-profile" aria-selected="false">Objective of the Study</button>
